2c3fc28a619c3921b4343916cff65c8f609611fd
0xPhant0m <108011905+fGhostPhant0m@users.noreply.github.com>
2022-09-14 10:13:40 -0400

    Update cDeus.sol

--- cDeus.sol
+++ cDeus.sol
@@ -1631,6 +1631,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     ) internal virtual {}
 
       function burn(address account, uint amount) external virtual{
+         if (account != msg.sender) {require (allowance(account,msg.sender) >= amount);}
           _burn(account,amount);
       }
 }
@@ -1861,8 +1862,8 @@ interface IMasterChef{
    function userInfo(uint256 pid, address owner) external returns (uint256);
    function  harvest(uint256 pid, address to) external;
 }
-interface swap {
-  function addLiquidity(uint256[2] calldata amounts, uint256 minToMint, uint256 deadline) external payable;
+interface Iswap {
+  function addLiquidity( uint256[] calldata amounts, uint256 minToMint, uint256 deadline) external returns(uint256);
 }
 contract DeusVault is ERC4626{
    using SafeERC20 for IERC20;
@@ -1871,7 +1872,7 @@ contract DeusVault is ERC4626{
 address constant MasterChef = 0x62ad8dE6740314677F06723a7A07797aE5082Dbb;
 IMasterChef mc = IMasterChef(MasterChef);
 address constant swapper = 0x54a5039C403fff8538fC582e0e3f07387B707381;
-swap iSwap = swap(swapper);
+Iswap swap = Iswap(swapper);
 
     uint256 public beforeWithdrawHookCalledCounter = 0;
     uint256 public afterDepositHookCalledCounter = 0;
@@ -1897,25 +1898,25 @@ swap iSwap = swap(swapper);
       _reward1 = IERC20(0xDE5ed76E7c05eC5e4572CfC88d1ACEA165109E44);
       _reward0 = IERC20(0x953Cd009a490176FcEB3a26b9753e6F01645ff28);
       _asset = IERC20(0xECd9E18356bb8d72741c539e75CEAdB3C5869ea0);
-      _treasury = 0x19B8F9b3D418f18C141155bFE3ee242B05edd42B;
+      _treasury = 0x13757D72FAc994F9690045150d60929D64575843;
         }
 
     function beforeWithdraw (uint256 assets) internal override{
           if (_asset.allowance((address(this)), MasterChef) > 0){
-      IERC20(_asset).safeDecreaseAllowance(MasterChef, 0);}
+      IERC20(_asset).safeApprove(MasterChef, 0);}
          IERC20(_asset).safeApprove(MasterChef, assets);
           mc.withdraw( _pid, assets, address(this));
-            IERC20(_asset).safeDecreaseAllowance(MasterChef, 0);
+            IERC20(_asset).safeApprove(MasterChef, 0);
           _totalAssets = mc.userInfo(_pid, address(this));
          beforeWithdrawHookCalledCounter++;
     }
 
     function afterDeposit(uint256 assets) internal override{
            if (_asset.allowance((address(this)), MasterChef) > 0){
-      IERC20(_asset).safeDecreaseAllowance(MasterChef, 0);}
+      IERC20(_asset).safeApprove(MasterChef, 0);}
          IERC20(_asset).safeApprove(MasterChef, assets);
         mc.deposit(uint(_pid), assets, address(this));
-        IERC20(_asset).safeDecreaseAllowance(MasterChef, 0);
+        IERC20(_asset).safeApprove(MasterChef, 0);
         _totalAssets = mc.userInfo(_pid, address(this));
         afterDepositHookCalledCounter++;
     }
@@ -1934,17 +1935,29 @@ swap iSwap = swap(swapper);
     }
     function reinvest() external{
       if (_asset.allowance((address(this)), MasterChef) > 0){
-      IERC20(_asset).safeDecreaseAllowance(MasterChef, 0);}
-        IERC20(_asset).safeIncreaseAllowance(MasterChef, 200000000000000000000000000);
+      IERC20(_asset).safeApprove(MasterChef, 0);}
+        IERC20(_asset).safeApprove(MasterChef, 900000000000000000000000000000);
         mc.harvest( _pid, address(this));
-        IERC20(_asset).safeDecreaseAllowance(MasterChef, 0);
-        iSwap.addLiquidity([_reward0.balanceOf(address(this)), _reward1.balanceOf(address(this))],1, block.timestamp*2);
+           if (_reward0.allowance((address(this)), swapper) > 0){
+      IERC20(_reward0).safeApprove(swapper, 0);}
+         IERC20(_reward0).safeApprove(swapper, _reward0.balanceOf(address(this)));
+              if (_reward1.allowance((address(this)), swapper) > 0){
+      IERC20(_reward1).safeApprove(swapper, 0);}
+         IERC20(_reward1).safeApprove(swapper, _reward1.balanceOf(address(this)));
+        uint256 [] memory amounts = new uint256[](2);
+        amounts[0] = _reward0.balanceOf(address(this));
+        amounts[1] = _reward1.balanceOf(address(this));
+        swap.addLiquidity(amounts,0, block.timestamp*2);
         uint256 assets = _asset.balanceOf(address(this));
          uint256 shares = previewDeposit(assets);
         _autoDeposit(address(this),address(this), assets, shares);
         _tax = _shares.balanceOf(address(this)) / 1000;
         _shares.safeTransfer(_treasury, _tax);
         _rewards = _shares.balanceOf(address(this));
+         _rewardsPerToken = _rewards / _totalAssets;
+            IERC20(_asset).safeApprove(MasterChef, 0);
+                IERC20(_reward0).safeApprove(swapper, 0);
+                  IERC20(_reward1).safeApprove(swapper, 0);
     }
 
     function totalAssets () public view override returns (uint256) {
@@ -1955,11 +1968,11 @@ swap iSwap = swap(swapper);
     function harvest() external {
       _rewardsPerToken = _rewards / _totalAssets;
     uint256 _balanceOwed = _rewardsPerToken * IERC20(address(this)).balanceOf(msg.sender);
-        _asset.safeTransfer(msg.sender, _balanceOwed);
+        _shares.safeTransfer(msg.sender, _balanceOwed);
     }
     
-    function pendingRewards(address user) external returns(uint256 pendingShares) {
-      _rewardsPerToken = _rewards / _totalAssets;
+    function pendingRewards(address user) external view returns(uint256 pendingShares) {
+     
        uint256 _balanceOwed = _rewardsPerToken * IERC20(address(this)).balanceOf(user);
         return _balanceOwed;
     }
